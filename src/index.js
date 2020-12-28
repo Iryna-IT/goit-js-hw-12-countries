eb6e5371556413e24276a20b90f0b9108544da0e
Iryna Redka <redkaiv@gmail.com>
2020-12-28 07:37:03 +0200

    completed

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import './styles.css';
+// import './js/notification.js';
+
+
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+import { alert, notice, info, success, error } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
+
+import fetchCountries from './js/fetchCountries.js';
+import countriesListTemplate from './templates/countriesList.hbs';
+import countryInfoTemplate from './templates/countryInfo.hbs';
+
+
+const debounce = require('lodash.debounce');
+
+const inputRef = document.querySelector('input');
+const countryRef = document.querySelector(".country");
+
+
+
+
+inputRef.addEventListener('input', debounce(renderCountryDescriprion, 500));
+
+
+
+
+function renderCountryDescriprion(event) {
+  countryRef.innerHTML = "";
+  let searchQuery = event.target.value;
+
+    fetchCountries(searchQuery).then(data => {
+    const markup = countryInfoTemplate(data);
+    const countriesList = countriesListTemplate(data);
+    if (!data) {
+      return;
+    } else if (data.length > 10) {
+      const errorNotification = error({
+  text: "Too many matches found. Please, enter a correct country name!"
+      });
+      return errorNotification;
+    } else if (data.length === 1) {
+      countryRef.insertAdjacentHTML('beforeend', markup);
+      inputRef.value = "";
+    }else if (data.length >= 2 && data.length <= 10) {
+      countryRef.insertAdjacentHTML('beforeend', countriesList);
+
+      if (countryRef !== "") {
+        const countriesItemRef = document.querySelectorAll(".counrties-item");
+        Array.prototype.map.call(countriesItemRef,
+        function (obj) {obj.addEventListener('click', renderNewCountryDescriprion); })
+        function renderNewCountryDescriprion(event) {
+          countryRef.innerHTML="";
+          searchQuery = event.currentTarget.textContent;
+
+        fetchCountries(searchQuery).then(data => {
+          const markup = countryInfoTemplate(data);
+          countryRef.insertAdjacentHTML('beforeend', markup);
+          searchQuery = "";
+        })
+      };
+        }
+    } else {
+      const alertNotification = alert({
+  text: "Nothing found. Please, enter a more specific query"
+});
+      return alertNotification;
+    }
+  }).catch(error => console.log(error))
+}
---
+++ src/js/notification.js
@@ -0,0 +1,12 @@
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+import { alert, notice, info, success, error } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
+
+//  export const alertNotification = alert({
+//   text: "Nothing found. Please, enter a more specific query"
+// });
+
+// export const errorNotification = error({
+//   text: "Too many matches found. Please, enter a correct country name!"
+// });
